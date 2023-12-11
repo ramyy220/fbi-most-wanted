15cc8e2bea9bfc5a509cc294ed543ef8aed672fe
Ramyy220 <ramyamr70@gmail.com>
2023-12-11 16:09:21 +0100

    HomePage

---
+++ fbi-most-wanted/screens/Account.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, Text} from 'react-native'
+import { StyleSheet } from 'react-native'
+
+const Account = () => {
+  return (
+    <View>
+      <Text>Account</Text>
+    </View>
+   
+  )
+}
+
+const styles = StyleSheet.create({
+    
+})
+export default Account
---
+++ fbi-most-wanted/screens/Like.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text} from 'react-native'
+import { StyleSheet } from 'react-native'
+
+const Like = () => {
+  return (
+    <View>
+      <Text>Like</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    
+})
+export default Like
