91d3c4d64bc00ac576e37a763d240cc8a3e0fad6
r-jb <24355753+r-jb@users.noreply.github.com>
2023-12-15 13:49:40 +0100

    account: fix buttons with keyboard up

--- screens/Account.js
+++ screens/Account.js
@@ -1,105 +1,104 @@
-import { useEffect, useState } from 'react';
-import { auth, firestore } from '../config/firebase';
-import { collection, query, where, getDoc, doc, updateDoc } from 'firebase/firestore';
-import { Image, StyleSheet, Text, View, Button, Alert } from 'react-native';
-import Logo from '../assets/Seal_of_the_FBI.svg.png';
-import * as ImagePicker from 'expo-image-picker';
-import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
-import CustomButton from '../components/CustomButton';
-import { TouchableOpacity } from 'react-native';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import CustomInput from '../components/CustomInput';
-import { signOut } from 'firebase/auth';
+import { useEffect, useState } from "react";
+import { auth, firestore } from "../config/firebase";
+import { collection, getDoc, doc, updateDoc } from "firebase/firestore";
+import { Image, StyleSheet, Text, View, Alert, ScrollView } from "react-native";
+import Logo from "../assets/Seal_of_the_FBI.svg.png";
+import * as ImagePicker from "expo-image-picker";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import CustomButton from "../components/CustomButton";
+import { TouchableOpacity } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import CustomInput from "../components/CustomInput";
+import { signOut } from "firebase/auth";
 
 const Account = ({ navigation }) => {
   const [user, setUser] = useState({
-    name: '',
-    email: '',
-    profileImageUrl: Logo
+    name: "",
+    email: "",
+    profileImageUrl: Logo,
   });
 
   useEffect(() => {
     async function getUser() {
-        const currentUser = auth.currentUser;
-        if (currentUser) {
-          const userCollection = collection(firestore, 'users');
-          const docref = doc(userCollection, currentUser.uid);
-          const docsnapshot = await getDoc(docref);
+      const currentUser = auth.currentUser;
+      if (currentUser) {
+        const userCollection = collection(firestore, "users");
+        const docref = doc(userCollection, currentUser.uid);
+        const docsnapshot = await getDoc(docref);
 
-          if(docsnapshot.exists()) {
-            const userDoc = docsnapshot;
-            const userData = userDoc.data();
-            console.log('User data:', userData);
-            setUser({
-              ...user,
-              newName: userData.username,
-              newEmail: userData.email,
-              profileImageUrl: { uri: userData.profileImageUrl }
-            });
-          }
-
-        }
-      }
-
-       getUser(); // Ajoutez cette ligne
-
-    }, []);
-
-
-    const pickImage = async () => {
-      try {
-        let result = await ImagePicker.launchImageLibraryAsync({
-          mediaTypes: ImagePicker.MediaTypeOptions.Images,
-          allowsEditing: true,
-          aspect: [1, 1],
-          quality: 1,
-        });
-    
-        if (!result.canceled) {
-          const imageUri = result.assets[0].uri;
-          const response = await fetch(imageUri);
-          const blob = await response.blob();
-    
-          const storage = getStorage();
-          const storageRef = ref(storage, 'user_profile_images/' + auth.currentUser.uid);
-    
-          await uploadBytes(storageRef, blob);
-    
-          const downloadURL = await getDownloadURL(storageRef);
-    
-          const userCollection = collection(firestore, 'users');
-          const docref = doc(userCollection, auth.currentUser.uid);
-          await updateDoc(docref, {
-            profileImageUrl: downloadURL
+        if (docsnapshot.exists()) {
+          const userDoc = docsnapshot;
+          const userData = userDoc.data();
+          console.log("User data:", userData);
+          setUser({
+            ...user,
+            newName: userData.username,
+            newEmail: userData.email,
+            profileImageUrl: { uri: userData.profileImageUrl },
           });
-    
-          setUser({ ...user, profileImageUrl: { uri: downloadURL } });
         }
-      } catch (error) {
-        console.error("An error occurred while picking the image: ", error);
-        Alert.alert("Error", "An error occurred while picking the image.");
       }
-    };
-    
+    }
 
-  const handleLogout = () => {
-    
-    signOut(auth).then(() => {
-      Alert.alert("Logout", "You have been disconnected.");
-      navigation.navigate('Login');
-  }).catch((error) => {
-      console.error("Logout error:", error);
-  });
+    getUser();
+  }, []);
+
+  const pickImage = async () => {
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [1, 1],
+        quality: 1,
+      });
+
+      if (!result.canceled) {
+        const imageUri = result.assets[0].uri;
+        const response = await fetch(imageUri);
+        const blob = await response.blob();
+
+        const storage = getStorage();
+        const storageRef = ref(
+          storage,
+          "user_profile_images/" + auth.currentUser.uid
+        );
+
+        await uploadBytes(storageRef, blob);
+
+        const downloadURL = await getDownloadURL(storageRef);
+
+        const userCollection = collection(firestore, "users");
+        const docref = doc(userCollection, auth.currentUser.uid);
+        await updateDoc(docref, {
+          profileImageUrl: downloadURL,
+        });
+
+        setUser({ ...user, profileImageUrl: { uri: downloadURL } });
+      }
+    } catch (error) {
+      console.error("An error occurred while picking the image: ", error);
+      Alert.alert("Error", "An error occurred while picking the image.");
+    }
   };
 
-  
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        Alert.alert("Logout", "You have been disconnected.");
+        navigation.navigate("Login");
+      })
+      .catch((error) => {
+        console.error("Logout error:", error);
+      });
+  };
+
   const handleUpdateUser = async () => {
-    const userCollection = collection(firestore, 'users');
+    const userCollection = collection(firestore, "users");
     const docref = doc(userCollection, auth.currentUser.uid);
     try {
       await updateDoc(docref, {
         username: user.newName,
-        email: user.newEmail
+        email: user.newEmail,
       });
       setUser({ ...user, name: user.newName, email: user.newEmail });
       Alert.alert("Success", "your profile has been updated");
@@ -107,45 +106,76 @@ const Account = ({ navigation }) => {
       Alert.alert("Error", "An error occurred during the update.");
     }
   };
-  
 
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={pickImage} style={styles.imageWrapper}>
-        <Image source={user.profileImageUrl} style={styles.profileImage} />
-        <MaterialCommunityIcons name="image-edit" size={35} color="#002176" style={styles.iconStyle} />
-      </TouchableOpacity> 
-      <CustomInput
-        icone={<MaterialCommunityIcons name="account-edit" size={20} color="#002176" />}
-        value={user.newName}
-        setValue={(text) => setUser({ ...user, newName: text })}
-        placeholder="New username"
-      />
-      <CustomInput
-        icone={<MaterialCommunityIcons name="email-edit" size={20} color="#002176" />}
-        value={user.newEmail}
-        setValue={(text) => setUser({ ...user, newEmail: text })}
-        placeholder="New email"
-      />
-      <TouchableOpacity onPress={handleUpdateUser} style={styles.updateButton}>
-        <Text style={styles.updateButtonText}>Upload your profile</Text>
-      </TouchableOpacity>
-      <View style={styles.logout}>
-      <CustomButton text="Logout" onPress={handleLogout} icone={<MaterialCommunityIcons name='logout-variant' size={20} color='white' />} /> 
-      </View>
+    <View style={{ flex: 1 }}>
+      <ScrollView keyboardShouldPersistTaps="always">
+        <View style={{ ...styles.container, justifyContent: "space-between" }}>
+          <TouchableOpacity onPress={pickImage} style={styles.imageWrapper}>
+            <Image source={user.profileImageUrl} style={styles.profileImage} />
+            <MaterialCommunityIcons
+              name="image-edit"
+              size={35}
+              color="#002176"
+              style={styles.iconStyle}
+            />
+          </TouchableOpacity>
+          <CustomInput
+            icone={
+              <MaterialCommunityIcons
+                name="account-edit"
+                size={20}
+                color="#002176"
+              />
+            }
+            value={user.newName}
+            setValue={(text) => setUser({ ...user, newName: text })}
+            placeholder="New username"
+          />
+          <CustomInput
+            icone={
+              <MaterialCommunityIcons
+                name="email-edit"
+                size={20}
+                color="#002176"
+              />
+            }
+            value={user.newEmail}
+            setValue={(text) => setUser({ ...user, newEmail: text })}
+            placeholder="New email"
+          />
+          <TouchableOpacity
+            onPress={handleUpdateUser}
+            style={styles.updateButton}
+          >
+            <Text style={styles.updateButtonText}>Upload your profile</Text>
+          </TouchableOpacity>
+          <View style={styles.logout}>
+            <CustomButton
+              text="Logout"
+              onPress={handleLogout}
+              icone={
+                <MaterialCommunityIcons
+                  name="logout-variant"
+                  size={20}
+                  color="white"
+                />
+              }
+            />
+          </View>
+        </View>
+      </ScrollView>
     </View>
-
-
   );
-  
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#e6eef8',
-    alignItems: 'center',
+    backgroundColor: "#e6eef8",
+    alignItems: "center",
     padding: 20,
+    justifyContent: "space-between",
   },
   profileImage: {
     width: 200,
@@ -153,70 +183,68 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     marginBottom: 30,
     borderWidth: 4,
-    borderColor: '#002176',
+    borderColor: "#002176",
   },
   imageWrapper: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   iconStyle: {
-    position: 'absolute',
+    position: "absolute",
     right: 0,
     bottom: 25,
     borderRadius: 15,
     padding: 5,
   },
   updateButton: {
-    backgroundColor: '#002176',
+    backgroundColor: "#002176",
     padding: 15,
     borderRadius: 10,
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 20,
   },
   updateButtonText: {
-    color: 'white',
+    color: "white",
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   userDetailsCard: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderRadius: 15,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
     padding: 20,
-    alignItems: 'center',
+    alignItems: "center",
   },
   nameText: {
     fontSize: 26,
-    fontWeight: 'bold',
-    color: '#002176',
+    fontWeight: "bold",
+    color: "#002176",
     marginBottom: 5,
   },
   emailText: {
     fontSize: 18,
-    color: '#333333',
+    color: "#333333",
     marginBottom: 20,
   },
   buttonStyle: {
-    backgroundColor: '#0044cc',
+    backgroundColor: "#0044cc",
     paddingVertical: 12,
     paddingHorizontal: 25,
     borderRadius: 25,
     marginTop: 10,
   },
   buttonText: {
-    color: 'white',
+    color: "white",
     fontSize: 16,
   },
   logout: {
-    marginTop: 'auto',
+    marginTop: "auto",
     marginBottom: 40,
-
   },
 });
-
 
 export default Account;
